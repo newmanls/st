f9fd3ec71908ad015143422dfd24a7602f64d8f5
newmanls <lrtech8@gmail.com>
2023-03-02 19:49:34 -0400

    patch: scrollback-0.8.5

--- config.h
+++ config.h
@@ -256,6 +256,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 	{ MODKEY,               XK_y,           copyurl,        {.i =  0} },
+	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
 };
 
 /*
